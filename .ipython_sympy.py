7587c98f3af39a04acb8d3d50d7b49a8dac5509f
Jythonscript <bartnik314@gmail.com>
2020-03-02 19:47:11 -0600

    add more symbols and functions

--- .ipython_sympy.py
+++ .ipython_sympy.py
@@ -2,8 +2,11 @@ import pyperclip
 from sympy import *
 init_printing()
 
-x,y,z = symbols('x, y, z')
-a,b,c = symbols('a, b, c')
+# symbols
+x, y, z = symbols('x y z')
+a, b, c = symbols('a b c')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
 t = symbols('t')
 phi,rho,theta = symbols('phi, rho, theta')
 
