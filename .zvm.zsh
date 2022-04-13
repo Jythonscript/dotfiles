6513a995cc9f73fb2e1d069d8e1522a8c4d8bc84
Jythonscript <bartnik314@gmail.com>
2022-04-13 12:39:36 -0500

    add zvm_config function

--- .zvm.zsh
+++ .zvm.zsh
@@ -1,13 +1,16 @@
-ZVM_KEYTIMEOUT=0.04
-
 function my_exit() {
 	exit
+}
+
+function zvm_config() {
+	ZVM_KEYTIMEOUT=0.04
 }
 
 function zvm_after_init() {
 	[ -f ~/.oh-my-zsh/plugins/fzf/fzf.plugin.zsh ] && source ~/.oh-my-zsh/plugins/fzf/fzf.plugin.zsh
 
 	zvm_define_widget vimbuffer
+
 	zvm_bindkey viins '^P' vimbuffer
 	zvm_bindkey viins '^Q' push-line
 	zvm_bindkey viins '^H' backward-kill-word
@@ -21,5 +24,4 @@ function zvm_after_lazy_keybindings() {
 	zvm_bindkey vicmd 'ZQ' my_exit
 	zvm_bindkey vicmd ',q' my_exit
 	zvm_bindkey vicmd ',w' accept-line
-	zvm_bindkey vicmd ',v' edit-command-line
 }
