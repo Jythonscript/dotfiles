8687d62048ca16be59ed613343da9747dcfb537f
Jythonscript <bartnik314@gmail.com>
2022-05-17 13:17:10 -0500

    add clipboard toggle shortcut

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,16 @@ require('plugins')
 
 vim.cmd('runtime nvimrc.vim')
 vim.cmd('runtime vimrc.vim')
+
+local toggle_clipboard = function()
+	local status = vim.api.nvim_get_option("clipboard")
+	if status == "unnamedplus" then
+		vim.api.nvim_set_option("clipboard", "")
+		print("Clipboard disabled")
+	else
+		vim.api.nvim_set_option("clipboard", "unnamedplus")
+		print("Clipboard enabled")
+	end
+end
+
+vim.keymap.set('n', '<leader>c', toggle_clipboard, {expr = true, noremap = true})
