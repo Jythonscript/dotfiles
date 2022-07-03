d1b2d1e2e528e66808de4c621f0757baf8a6f991
Jythonscript <bartnik314@gmail.com>
2022-07-03 14:22:38 -0500

    update nvim-cmp mappings

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -6,17 +6,13 @@ cmp.setup {
 			vim.fn["UltiSnips#Anon"](args.body)
 		end,
 	},
-	mapping = {
-		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-		['<C-y>'] = cmp.config.disable,
-		['<C-e>'] = cmp.mapping {
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		},
-		['<CR>'] = cmp.mapping.confirm { select = false },
-	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
+	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		--{ name = 'ultisnips' },
