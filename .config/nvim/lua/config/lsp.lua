3a4a9923a097f3691a7e3070a6d3599105ebb2b6
Jythonscript <bartnik314@gmail.com>
2025-07-06 18:20:08 -0500

    add nix lsp, update plugins

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -23,7 +23,7 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
 	}
 )
 
-local servers = { "clangd", "pyright" }
+local servers = { "clangd", "pyright", "nixd" }
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup { on_attach = on_attach }
 end
